f4451a046d361c3d6cd56389cb50b3a827da8de0
Deep patra <74497717+Deep-patra@users.noreply.github.com>
2023-11-04 17:24:58 +0530

    UPDATE: updated manifest file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,43 +1,45 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import Router from 'next/router'
-import Head from 'next/head'
-import { useState, useEffect } from 'react'
-import { ModeContext, useThemeMode } from '../hooks/theme_provider'
-import { useRegisterWorker } from '../hooks/registerWorker'
-import ProgressBar from '../components/ProgresssBar'
-
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Router from "next/router";
+import Head from "next/head";
+import { useState, useEffect } from "react";
+import { ModeContext, useThemeMode } from "../hooks/theme_provider";
+import { useRegisterWorker } from "../hooks/registerWorker";
+import ProgressBar from "../components/ProgresssBar";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const [isLoading, changeLoadingState] = useState<boolean>(false)
-  const [themeMode, changeMode] = useThemeMode()
+  const [isLoading, changeLoadingState] = useState<boolean>(false);
+  const [themeMode, changeMode] = useThemeMode();
 
   // Hook to register the service worker
-  useRegisterWorker()
+  useRegisterWorker();
 
   useEffect(() => {
-    const handleRouteChangeStart = (url: string) => (url !== Router.asPath) && changeLoadingState(true)
-    const handleRouteChangeComplete = (url: string) => (url !== Router.asPath) && changeLoadingState(false)
+    const handleRouteChangeStart = (url: string) =>
+      url !== Router.asPath && changeLoadingState(true);
+    const handleRouteChangeComplete = (url: string) =>
+      url !== Router.asPath && changeLoadingState(false);
 
-    Router.events.on("routeChangeStart", handleRouteChangeStart)
-    Router.events.on("routeChangeError", handleRouteChangeComplete)
-    Router.events.on("routeChangeComplete", handleRouteChangeComplete)
+    Router.events.on("routeChangeStart", handleRouteChangeStart);
+    Router.events.on("routeChangeError", handleRouteChangeComplete);
+    Router.events.on("routeChangeComplete", handleRouteChangeComplete);
 
     return () => {
-      Router.events.off("routeChangeStart", handleRouteChangeStart)
-      Router.events.off("routeChangeError", handleRouteChangeComplete)
-      Router.events.off("routeChangeComplete", handleRouteChangeComplete)
-    }
-  }, [])
+      Router.events.off("routeChangeStart", handleRouteChangeStart);
+      Router.events.off("routeChangeError", handleRouteChangeComplete);
+      Router.events.off("routeChangeComplete", handleRouteChangeComplete);
+    };
+  }, []);
 
   return (
     <ModeContext.Provider value={{ theme: themeMode, toggleTheme: changeMode }}>
       <Head>
-        <link rel="manifest" type="application/manifest+json" href="./manifest.json" />
+        {/** Web Manifest **/}
+        <link rel="manifest" type="application/json" href="/app.webmanifest" />
       </Head>
 
-      <ProgressBar/>
+      <ProgressBar />
       <Component {...pageProps} changeMode={changeMode} />
     </ModeContext.Provider>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,28 @@
-import Head from 'next/head'
-import { ModeContext } from '../hooks/theme_provider'
-import Header from '../components/Header'
-import Main from '../components/Main'
-import Navigation from '../components/Navigation'
-import SearchBar from '../components/SearchBar'
-import Container from '../components/MangaContainer'
-import Store from '../redux/store/home_store'
-import { Provider } from 'react-redux'
+import Head from "next/head";
+import { ModeContext } from "../hooks/theme_provider";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Navigation from "../components/Navigation";
+import SearchBar from "../components/SearchBar";
+import Container from "../components/MangaContainer";
+import Store from "../redux/store/home_store";
+import { Provider } from "react-redux";
 
 const Home = () => {
   return (
     <>
       <Head>
         <title>Manga Fam</title>
-        <meta name="description" content="From One piece to Dragon ball Z, read thousands of mangas." />
-        <meta name="keywords" content="Manga Fam"/>
+        <meta
+          name="description"
+          content="From One piece to Dragon ball Z, read thousands of mangas."
+        />
+        <meta name="keywords" content="Manga Fam" />
         <meta property="og:title" content="Manga Fam" />
-        <meta property="og:description" content="From One piece to Dragon ball Z read thousands of mangas." />
+        <meta
+          property="og:description"
+          content="Read thousands of mangas for free."
+        />
         <meta property="og:type" content="website" />
         <meta property="og:image" content="/manga_fam_og.jpg" />
       </Head>
@@ -26,10 +32,10 @@ const Home = () => {
             <>
               <Header mode={theme} toggleTheme={toggleTheme} />
               <Main>
-                <Navigation/>
+                <Navigation />
                 <div className="flex flex-col flex-nowrap min-w-0 w-full">
-                  <SearchBar/>
-                  <Container/>
+                  <SearchBar />
+                  <Container />
                 </div>
               </Main>
             </>
@@ -37,8 +43,7 @@ const Home = () => {
         </ModeContext.Consumer>
       </Provider>
     </>
-  )
-}
+  );
+};
 
-
-export default Home
+export default Home;
