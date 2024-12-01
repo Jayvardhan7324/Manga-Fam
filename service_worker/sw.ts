95b6d259f95b1371ce037e0078d3376664c31324
Deep patra <deeppatra1999@gmail.com>
2024-12-01 22:24:24 +0530

    change: updated the service worker

--- service_worker/sw.ts
+++ service_worker/sw.ts
@@ -1,3 +1,33 @@
-self.addEventListener("message", (event) => {
-  console.log("Received a message => ", event)
+const addResourcesToCache = async (resources: any) => {
+  const cache = await caches.open("v1")
+  await cache.addAll(resources)
+}
+
+const putInCache = async (request: Request, response: Response) => {
+  const cache = await caches.open("v1")
+  await cache.put(request, response)
+}
+
+const cacheFirst = async (request: Request) => {
+  const responseFromCache = await caches.match(request)
+
+  if (responseFromCache) return responseFromCache
+
+  const responseFromNetwork = await fetch(request)
+
+  await putInCache(request, responseFromNetwork)
+
+  return responseFromCache
+}
+
+self.addEventListener("install", (event: any) => {
+  event.waitUntil(addResourcesToCache(["*"]))
+})
+
+self.addEventListener("fetch", (event: any) => {
+  event.respondWith(cacheFirst(event.request))
+})
+
+self.addEventListener("activate", (event: any) => {
+  event.waitUntil(( self as any).registration?.navigationPreload.enable())
 })
